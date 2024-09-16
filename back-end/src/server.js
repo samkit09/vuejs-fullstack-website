7414bf5354e4e8981d35184292e3a36ead23cd13
samkit <sam@samsx.lan>
2024-09-16 17:07:35 -0500

    init

---
+++ back-end/src/server.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+(async function start() {
+  const username = process.env.mongodb_username;
+  const password = process.env.mongodb_password;
+  const url = `mongodb+srv://${username}:${password}@cluster0.jvrol.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const client = new MongoClient(url);
+  await client.connect();
+  const db = client.db('vuedb');
+
+  const app = express();
+  app.use(express.json());
+  app.use('/image', express.static(path.join(__dirname, '../assets')));
+
+  // End point to get list of products
+  app.get('/api/products', async (req, res) => {
+    const products = await db.collection("products").find({}).toArray();
+    res.send(products);
+  });
+
+  // Helper function 
+  async function populatedCartIds(ids) {
+    return Promise.all(ids.map(id => db.collection("products").findOne({ id })));
+  }
+
+  // Endpoint for getting list of cart items of a particular user
+  app.get('/api/users/:userId/cart', async (req, res) => {
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populatedCartIds(user?.cartItems || []);
+    res.json(populatedCart);
+  });
+
+  // Endponit to get a particular product from the list
+  app.get('/api/products/:productId', async (req, res) => {
+    const productId = req.params.productId;
+    const product = await db.collection('products').findOne({ id: productId });
+    res.json(product);
+  });
+
+  // Endpoint for adding a product to cart of the particular user
+  app.post('/api/users/:userId/cart', async (req, res) => {
+    const userId = req.params.userId;
+    const productId = req.body.id;
+
+    const existingUser = await db.collection('users').findOne({ id: userId });
+    if (!existingUser) {
+      await db.collection('users').insertOne({ id: userId, cartItems: [] });
+    }
+
+    db.collection('users').updateOne({ id: userId }, {
+      $addToSet: { cartItems: productId }
+    })
+
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populatedCartIds(user?.cartItems || []);
+    res.json(populatedCart);
+  });
+
+  // Endpoint for deleting a particular product from the cart
+  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+    const userId = req.params.userId;
+    const productId = req.params.productId;
+
+    await db.collection('users').updateOne({ id: userId }, {
+      $pull: { cartItems: productId }
+    });
+
+    const user = await db.collection('users').findOne({ id: userId });
+    const populatedCart = await populatedCartIds(user?.cartItems || []);
+    res.json(populatedCart);
+  });
+
+  app.listen(8000, () => {
+    console.log("Server is Listening on port 8000...");
+  });
+
+})();
---
+++ front-end/src/pages/ProductsPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Products</h1>
+  <ProductsList :products="products"/>
+</template>
+
+<script>
+import ProductsList from '@/components/ProductsList.vue';
+import axios from 'axios';
+
+export default {
+  name: "ProductsPage",
+  components: {
+    ProductsList,
+  },
+  props: ['user'],
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async created() {
+    const response = await axios.get('/api/products');
+    const products = response.data;
+    this.products = products;
+  }
+}
+</script>
